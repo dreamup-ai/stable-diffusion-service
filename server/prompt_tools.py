62dffb7018d10105c28f9bf1015ad4fd16dea567
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2023-07-09 15:51:58 -0400

    with torch.no_grad

--- server/prompt_tools.py
+++ server/prompt_tools.py
@@ -1,5 +1,6 @@
 import math
 import re
+import torch
 
 
 def make_enclosures_symmetrical(prompt, open_char="(", close_char=")"):
@@ -9,17 +10,22 @@ def make_enclosures_symmetrical(prompt, open_char="(", close_char=")"):
     removing open or close characters to the beginning or end of the enclosed subprompts.
     """
     new_prompt = prompt
-    subprompt_finder = re.compile(f"({re.escape(open_char)}+)([^{re.escape(close_char)}]+)({re.escape(close_char)}+)", re.MULTILINE | re.IGNORECASE)
+    subprompt_finder = re.compile(
+        f"({re.escape(open_char)}+)([^{re.escape(close_char)}]+)({re.escape(close_char)}+)",
+        re.MULTILINE | re.IGNORECASE,
+    )
     subprompts = subprompt_finder.findall(prompt)
     for open_chars, subprompt, close_chars in subprompts:
         if len(open_chars) == len(close_chars):
             continue
-        
+
         if len(open_chars) > len(close_chars):
             new_subprompt = (open_char * len(close_chars)) + subprompt + close_chars
         else:
             new_subprompt = open_chars + subprompt + (close_char * len(open_chars))
-        new_prompt = new_prompt.replace(open_chars + subprompt + close_chars, new_subprompt)
+        new_prompt = new_prompt.replace(
+            open_chars + subprompt + close_chars, new_subprompt
+        )
     return new_prompt
 
 
@@ -27,7 +33,10 @@ def make_enclosures_symmetrical(prompt, open_char="(", close_char=")"):
 def rewrite_a1111_style_weights(prompt):
     prompt = make_enclosures_symmetrical(prompt, "(", ")")
     prompt = make_enclosures_symmetrical(prompt, "[", "]")
-    def rewrite_for_char(prompt, open_char="(", close_char=")", max_chars=5, weight_basis=1.1):
+
+    def rewrite_for_char(
+        prompt, open_char="(", close_char=")", max_chars=5, weight_basis=1.1
+    ):
         # Iterate over the maximum number of modifier characters downwards
         for num_chars in range(max_chars, 0, -1):
             open = open_char * num_chars
@@ -59,11 +68,12 @@ def rewrite_a1111_style_weights(prompt):
                         weight = 1
                     else:
                         weight = float(weight)
-                    
-                
+
                 # Replace the prompt with the nataili-style prompt
                 if weight is not None:
-                    prompt = prompt.replace(open + og_subprompt + close, f"({subprompt}:{weight:.2f})")
+                    prompt = prompt.replace(
+                        open + og_subprompt + close, f"({subprompt}:{weight:.2f})"
+                    )
 
                 # Find next subprompt
                 subprompt_open_i = prompt.find(open, subprompt_open_i + 1)
@@ -71,25 +81,31 @@ def rewrite_a1111_style_weights(prompt):
         return prompt
 
     # Rewrite for ( and ) trains
-    prompt = rewrite_for_char(prompt, open_char="(", close_char=")", max_chars=5, weight_basis=1.1)
+    prompt = rewrite_for_char(
+        prompt, open_char="(", close_char=")", max_chars=5, weight_basis=1.1
+    )
     # Rewrite for [ and ] trains
-    prompt = rewrite_for_char(prompt, open_char="[", close_char="]", max_chars=5, weight_basis=0.9)
+    prompt = rewrite_for_char(
+        prompt, open_char="[", close_char="]", max_chars=5, weight_basis=0.9
+    )
 
     return prompt
 
 
 nat_weights = re.compile("\((.+?):([\d\.]+)\)", re.MULTILINE | re.IGNORECASE)
+
+
 def rewrite_prompt_for_compel(prompt):
     prompt = rewrite_a1111_style_weights(prompt)
 
     # convert the prompt weighting syntax from `a tall (man:1.1) picking apricots` to `a tall (man)1.1 picking apricots`
     prompt = nat_weights.sub(r"(\1)\2", prompt)
-   
-    
+
     return prompt
 
 
 def get_prompt_embeds(compel, prompt):
     prompt = rewrite_prompt_for_compel(prompt)
-    conditioning = compel.build_conditioning_tensor(prompt)
+    with torch.no_grad():
+        conditioning = compel.build_conditioning_tensor(prompt)
     return conditioning
